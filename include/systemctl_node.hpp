d938dc4f1da19d99dfe0fe5b75bd1413cd35bf5b
Fixit-Davide <graziato.davide@outlook.it>
2023-12-06 17:40:02 +0100

    Uncrustify
    (cherry picked from commit 26d82f265e6195f43b6aba18f08e590345a87fae)

--- include/systemctl_node.hpp
+++ include/systemctl_node.hpp
@@ -5,38 +5,40 @@
 #include "std_srvs/srv/trigger.hpp"
 
 
-namespace addons {
+namespace addons
+{
 
-	class SystemctlController : public rclcpp::Node {
-		public:
-			
-			SystemctlController(const rclcpp::NodeOptions & options);
-			
-			~SystemctlController(){};
+class SystemctlController : public rclcpp::Node
+{
+public:
+  SystemctlController(const rclcpp::NodeOptions & options);
 
-			void service_call(const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
-                                        std::shared_ptr<std_srvs::srv::Trigger::Response> response,
-                                        const std::string &method,
-                                        std::string sys_ser); 
+  ~SystemctlController() {}
 
-			void query_status(const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
-                        std::shared_ptr<std_srvs::srv::Trigger::Response> response,
-                        const std::string &property,
-                        std::string sys_res);
+  void service_call(
+    const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
+    std::shared_ptr<std_srvs::srv::Trigger::Response> response,
+    const std::string & method,
+    std::string sys_ser);
 
-    private:
+  void query_status(
+    const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
+    std::shared_ptr<std_srvs::srv::Trigger::Response> response,
+    const std::string & property,
+    std::string sys_res);
 
-      std::string start_service_name_;
-      std::string stop_service_name_;
-      std::string restart_service_name_;
-      std::string query_service_name_;
-      std::vector<std::string> sys_services_list;
+private:
+  std::string start_service_name_;
+  std::string stop_service_name_;
+  std::string restart_service_name_;
+  std::string query_service_name_;
+  std::vector<std::string> sys_services_list;
 
-      std::vector<rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr> start_srvs_;
-      std::vector<rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr> stop_srvs_;
-      std::vector<rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr> restart_srvs_;
-      std::vector<rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr> query_srvs_;
-  };
+  std::vector<rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr> start_srvs_;
+  std::vector<rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr> stop_srvs_;
+  std::vector<rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr> restart_srvs_;
+  std::vector<rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr> query_srvs_;
+};
 } // namespace addons
 
 
